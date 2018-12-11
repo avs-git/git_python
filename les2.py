31ce7fb062162ebb22e38e5d70e43eef7ed7d11c
Andrey <schmidtav@yandex.ru>
2018-12-11 17:56:42 +0300

    первый раз загружаем

---
+++ les2.py
@@ -0,0 +1,54 @@
+# coding UTF-8
+
+import turtle
+import random
+import math
+
+PHI = 360 / 7
+R = 50
+
+def gotoxy(x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+
+def make_circle(x, y, radius, color):
+    gotoxy(x, y)
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+
+def create_pistol(x, y):
+    make_circle(x, y, 80, 'white')
+    make_circle(x, y + 160, 5, 'red')
+    for i in range(0, 7):
+        phi_rad = PHI * i * math.pi / 180.0
+        make_circle(math.sin(phi_rad) * R, math.cos(phi_rad) * R + 60, 22, 'white')
+
+def make_turn(start, turns):
+    for i in range(start, turns):
+        phi_rad = PHI * i * math.pi / 180.0
+        make_circle(math.sin(phi_rad) * R, math.cos(phi_rad) * R + 60, 22, 'brown')
+        make_circle(math.sin(phi_rad) * R, math.cos(phi_rad) * R + 60, 22, 'white')
+    make_circle(math.sin(phi_rad) * R, math.cos(phi_rad) * R + 60, 22, 'brown')
+    if i % 7 == 0:
+        gotoxy(-150, 250)
+        turtle.write("Вы проиграли!", font = ("Arial", 18, "normal"))
+    else:
+        gotoxy(-150, 250)
+        turtle.write("Повезло!", font=("Arial", 18, "normal"))
+    return i % 7
+
+
+turtle.speed(0)
+
+create_pistol(0, 0)
+answer = ''
+start = 0
+while answer != 'n':
+    answer = turtle.textinput('Играем','y/n')
+    start = make_turn(start, random.randrange(7, 35))
+
+
+
